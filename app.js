ed43e4e1bc0c9a020b84de03f8a5d74abb544fbe
Jolppa <jolppa96@gmail.com>
2021-06-21 22:33:27 +0300

    Project layout done

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
 
 const app = express();
+
+// Configure static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine(".hbs", exphbs({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+app.use("/", require("./routes/routes"));
 
 app.listen(3000, () => console.log("Server Running..."));
